85a19ea639131ab2c6b4c8ff0573e0ca556c7eb3
Vinicius Mello <vmmello@webenabled.com>
2013-08-16 15:04:46 -0300

    extplorer: added auto login

--- compat/w_/public_html/cgi/extplorer.php
+++ compat/w_/public_html/cgi/extplorer.php
@@ -1,40 +1,55 @@
-#!/bin/bash
-#<?php $args = array(getenv('SCRIPT_FILENAME')); pcntl_exec('/bin/bash', $args); ?>
+<?php
 
-PMA_BASE="$SCRIPT_NAME"
-PMA_ROOT=/opt/webenabled/compat/w_/public_html/extplorer
+$curr_path = dirname(__FILE__);
+require_once($curr_path . "/../../../../lib/php/webapp_token_access.inc.php");
+$extplorer_dir = sprintf("%s/../extplorer/current", $curr_path);
 
-protocol=http
-[ o"$HTTPS" = o"on" ] && protocol=https
+$app_name = "extplorer";
+session_name($app_name);
 
-SCRIPT_NAME="$PMA_BASE$PATH_INFO"
-SCRIPT_URL="$SCRIPT_NAME"
-SCRIPT_URI="$protocol://$SERVER_NAME$SCRIPT_URL"
-SCRIPT_FILENAME="$PMA_ROOT/${SCRIPT_NAME##*/}"
-REQUEST_URI="$SCRIPT_URL"
-# dirty hack against --force-cgi-redirect
-export REDIRECT_STATUS=200
+$vhost = dp_derive_gen_vhost();
 
-# if PATH_INFO is empty, redirect
-if [ -z "$PATH_INFO" ]; then
-	echo "Refresh: 0; url=$SCRIPT_URI/index.php"
-	echo 'Content-type: text/html'
-	echo
-	cat << EOF 
-<html>
-<head>
-<title>extplorer re-direction</title>
-<meta http-equiv="Refresh" content="0; url=$SCRIPT_URI/index.php" />
-</head>
-<body>
-<h1>Redirecting ...</h1>
-<p>The proper page to access extplorer is located at <a href="$SCRIPT_URI/index.php">$SCRIPT_URI/index.php</a>.</p>
-</body>
-EOF
-exit 0
-fi
+$is_logged_in = dp_is_already_logged_to_app($app_name);
 
-unset PATH_INFO PATH_TRANSLATED
-export SCRIPT_NAME SCRIPT_URL SCRIPT_URI SCRIPT_FILENAME REQUEST_URI
-cd "$PMA_ROOT"
-exec /opt/webenabled/config/os/pathnames/bin/php-cgi "$SCRIPT_FILENAME"
+if(!$is_logged_in && !($token = dp_get_app_token_from_url())) {
+  echo "Access denied. Unable to verify app token.\n";
+  exit(1);
+} else if(!$is_logged_in && dp_has_valid_token($vhost, $app_name, $token)) {
+  dp_start_app_session($vhost, $app_name);
+  header('Location: ' . str_replace("/$token", "", $_SERVER['SCRIPT_URI']) . '/index.php');
+  exit;
+} else if($is_logged_in && dp_get_app_token_from_url()) {
+  $file = 'index.php';
+}
+
+$file = isset($file) ? $file : basename($_SERVER['PATH_INFO']);
+
+if ($file == '') {
+        header('Location: ' . str_replace("/$token", "", $_SERVER['SCRIPT_URI']) . '/index.php');
+        exit;
+}
+
+if ($file == 'index.php')
+{
+	if (isset($_GET['action']) && $_GET['action'] == 'logout')
+		setcookie('we_logout');
+
+	if (isset($_GET['mode']) && $_GET['mode'] == '1')
+		$_SESSION['xtp_wrapper_advanced_user'] = true;
+	elseif (empty($_GET['mode']))
+		$_SESSION['xtp_wrapper_advanced_user'] = false;
+}
+
+$home = isset($_ENV['XTP_HOME']) ? $_ENV['XTP_HOME'] : '';
+if ($home == '')
+{
+	$user_info = posix_getpwuid(posix_geteuid());
+	$home = $user_info['dir'];
+}
+
+$_SERVER['DOCUMENT_ROOT'] = $home;
+
+chdir($extplorer_dir);
+require_once($file); //($file == '' ? 'index.php' : $file));
+
+?>
