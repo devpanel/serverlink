29885e3a49389c3189370d40a5fc6a50676070bf
Vinicius Mello <vmmello@webenabled.com>
2013-10-04 15:31:41 -0300

    bootstrap.centos: fixed missing git pkg install

--- install/bootstrap/bootstrap.centos.sh
+++ install/bootstrap/bootstrap.centos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 bootstrap_centos() {
-  yum -y install perl perl-devel perl-CGI perl-Time-HiRes \
+  yum -y install git perl perl-devel perl-CGI perl-Time-HiRes \
     perl-Digest-HMAC perl-Digest-SHA perl-Net-SSLeay
-
 }
