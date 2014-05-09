7bdaa120eb3cda1cd7d0039bd5fdf8038e3d33b5
Vinicius Mello <vmmello@webenabled.com>
2014-05-09 10:09:09 -0300

    install/install.ubuntu.sh: added apache2-utils

--- install/install.ubuntu.sh
+++ install/install.ubuntu.sh
@@ -24,7 +24,7 @@ ubuntu_install_distro_packages() {
 
   for i in \
     curl apache2 libapache2-mod-macro apache2-suexec zlib1g libapache2-mod-fcgid \
-    mysql-server git subversion \
+    mysql-server git subversion apache2-utils \
     php5 php5-cli php-pear php5-gd php5-curl php5-mysql \
     php5-cgi php5-mcrypt php5-sqlite libjson-xs-perl libcrypt-ssleay-perl libcgi-session-perl
   do
