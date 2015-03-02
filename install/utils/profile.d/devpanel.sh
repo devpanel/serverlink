c21696ce0bb025d83153a335a63dc0140e5b9d67
Vinicius Mello <vmmello@webenabled.com>
2015-03-02 20:04:49 -0300

    utils/profile.d/devpanel.sh: fixed PATH warning
    previously was using PATH+="..." which generated a warning on Debian.
    
    Also moved devPanel to the first in PATH, to have more precedence.

--- install/utils/profile.d/devpanel.sh
+++ install/utils/profile.d/devpanel.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#######
+# PATH
+#
+# add custom directories to PATH
 self_bin=`readlink -e "${BASH_SOURCE[0]}"`
 curr_dir=`dirname "$self_bin"`
 bin_path=`readlink -e "$curr_dir/../../../bin/.path"`
 bin_path_status=$?
 
 if [ $bin_path_status -eq 0 ] && ! [[ "$PATH" =~ :?$bin_path:? ]]; then
-  PATH+=":$bin_path"
+  PATH="$bin_path:$PATH"
 fi
 
 /bin/true
