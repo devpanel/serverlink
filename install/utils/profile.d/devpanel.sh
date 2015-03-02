c2dad5030a07f46327eb506c071b17d57c1b28e4
Vinicius Mello <vmmello@webenabled.com>
2015-03-02 20:09:04 -0300

    utils/profile.d/devpanel.sh: added lines for drush
    added lines to better support drush, when there are links for ~/bin/php-cli,
    ~/bin/php-cgi or ~/bin/php.

--- install/utils/profile.d/devpanel.sh
+++ install/utils/profile.d/devpanel.sh
@@ -13,4 +13,25 @@ if [ $bin_path_status -eq 0 ] && ! [[ "$PATH" =~ :?$bin_path:? ]]; then
   PATH="$bin_path:$PATH"
 fi
 
+
+########
+# Drush
+########
+#
+# if there's a custom PHP version set on user home, then
+# set drush related variables to use it
+home_php_cli="$HOME/bin/php-cli"
+home_php_cgi="$HOME/bin/php-cgi"
+home_php="$HOME/bin/php"
+
+for php_n in "$home_php_cli" "$home_php_cgi" "$home_php"; do
+  if [ -f "$php_n" -a -x "$php_n" ]; then
+    export BIN_PHP="$php_n" # backwards compatibility with 
+                            # some old drupal cron scripts
+
+    export DRUSH_PHP="$php_n"
+    break
+  fi
+done
+
 /bin/true
