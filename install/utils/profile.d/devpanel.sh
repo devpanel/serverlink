6d7021a17952793617b1d0ddece1303cc960b0f1
Vinicius Mello <vmmello@webenabled.com>
2015-07-28 20:46:31 -0300

    drush: removed old attempt to fix problem of previous commit

--- install/utils/profile.d/devpanel.sh
+++ install/utils/profile.d/devpanel.sh
@@ -20,24 +20,4 @@ if [ -n "$DEVPANEL_HOME" ] && ! echo "$PATH" | egrep -q ":?$DEVPANEL_HOME:?"; th
   PATH="$DEVPANEL_PATH:$PATH"
 fi
 
-########
-# Drush
-########
-#
-# if there's a custom PHP version set on user home, then
-# set drush related variables to use it
-home_php_cli="$HOME/bin/php-cli"
-home_php_cgi="$HOME/bin/php-cgi"
-home_php="$HOME/bin/php"
-
-for php_n in "$home_php_cli" "$home_php_cgi" "$home_php"; do
-  if [ -f "$php_n" -a -x "$php_n" ]; then
-    export BIN_PHP="$php_n" # backwards compatibility with 
-                            # some old drupal cron scripts
-
-    export DRUSH_PHP="$php_n"
-    break
-  fi
-done
-
 /bin/true
