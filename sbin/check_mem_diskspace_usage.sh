c4a9d2fa7306b3154510e6f16389219b55e72a8b
ponchov <alfonsovaldes84@gmail.com>
2016-02-25 13:10:05 -0600

    added nice lvl 19 to have this command as low priority

--- sbin/check_mem_diskspace_usage.sh
+++ sbin/check_mem_diskspace_usage.sh
@@ -11,12 +11,12 @@ for username in ${ARRAY[*]}; do
   procs=${#RSS_ARR[@]}
 
   # disk space usage
-  user_diskspace_usage=`du -s /home/clients/websites/${username}/|awk '{print $1}'`
+  user_diskspace_usage=`nice -n 19 du -s /home/clients/websites/${username}/|awk '{print $1}'`
 
   # db space usage
   db=$(sed 's/w_//g' <<< $username)
-  db_size=`du -s /home/clients/databases/b_${db}/|awk '{print $1}'`
-  logs_size=$(du -s /home/clients/websites/$username/logs/ | awk '{print $1}')
+  db_size=`nice -n 19 du -s /home/clients/databases/b_${db}/|awk '{print $1}'`
+  logs_size=$(nice -n 19 du -s /home/clients/websites/$username/logs/ | awk '{print $1}')
 
   echo "$username,$(((user_diskspace_usage + db_size) / 1024)),$((mem / 1024)),$procs,$((logs_size / 1024)),$((db_size / 1024))"
 done
