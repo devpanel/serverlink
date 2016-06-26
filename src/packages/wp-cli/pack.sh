e4b2ee03e22708d2b22181c548f547cd092c3ca9
Vinicius Mello <vmmello@webenabled.com>
2016-06-26 15:21:45 -0300

    update-packages: major improvements
    * each package has a serial number (plain integer) that identifies whether
      the package needs to be updated or not (if the serial is greater than the
      current one, then it needs to be updated)
    
    * remove files from previous installation (on update it verifies the files
      from previous installation that are not in the new package and remove it)
    
    * sha512 sum of package files are now verified to ensure that the file
      downloaded is equal to the one in the server
    
    * added script libexec/pack-package to help with packing of files

--- src/packages/wp-cli/pack.sh
+++ src/packages/wp-cli/pack.sh
@@ -12,6 +12,14 @@ usage() {
 
 version="$1"
 
+self_bin=$(readlink -e "$0")
+if [ $? -ne 0 ]; then
+  echo "Error: unable to determine self path" 1>&2
+  exit 1
+fi
+self_dir=${self_bin%/*}
+sys_dir=${self_dir%/*/*/*}
+
 temp_dir=$(mktemp -d)
 
 pack_dir="$temp_dir/pack"
@@ -38,5 +46,7 @@ ln -s "${target_file##*/}" "$main_pkg_dir/wp-cli"
 
 ln -s "../${main_pkg_dir#$pack_dir/bin/}/wp-cli" "$path_dir/wp-cli"
 ln -s "../${main_pkg_dir#$pack_dir/bin/}/wp-cli" "$path_dir/wp"
+
+"$sys_dir/libexec/pack-package" -d "$pack_dir" "wp-cli-$version.tar.gz" .
 
 echo "Inspect: $temp_dir"
