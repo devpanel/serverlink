b4e257a83bb046d95e4379f605559ca35cc77939
Vinicius Mello <vmmello@webenabled.com>
2016-06-27 00:43:47 -0300

    phpmyadmin: better config + fixes to broken images/css/js
    Added a smarter configuration scheme by including a file directly through
    config.inc.php (instead of wrapping all requests).
    
    The previous wrapper broke javascript and css and it was being very
    difficult to workaround the problems. By including through config.inc.php we
    control all requests in a very non-intrusive way (request-wise).

--- compat/w_/public_html/cgi/phpmyadmin.php
+++ compat/w_/public_html/cgi/phpmyadmin.php
@@ -10,7 +10,15 @@ $vhost = dp_derive_gen_vhost();
 
 $is_logged_in = dp_is_already_logged_to_app($app_name);
 
-if(!$is_logged_in) {
+if($is_logged_in) {
+  $token = dp_get_app_token_from_url();
+  if(empty($token)) {
+    header('Location: ' . $_SERVER['SCRIPT_URI'] . '/index.php');
+  } else {
+    header('Location: ' . str_replace("/$token", "", $_SERVER['SCRIPT_URI']) . '/index.php');
+  }
+  exit;
+} else {
   if(($token = dp_get_app_token_from_url()) && dp_has_valid_token($vhost, $app_name, $token)) {
     dp_start_app_session($vhost, $app_name, $token);
     header('Location: ' . str_replace("/$token", "", $_SERVER['SCRIPT_URI']) . '/index.php');
@@ -21,32 +29,4 @@ if(!$is_logged_in) {
   }
 }
 
-if(isset($_SERVER["PATH_INFO"])) {
-  if($_SERVER["PATH_INFO"] == "/" . $_SESSION["token"]) {
-    $file = "index.php";
-  } else {
-    $file = $_SERVER["PATH_INFO"];
-  }
-} else {
-  header('Location: ' . str_replace("/$token", "/", $_SERVER['SCRIPT_URI']) . '/index.php');
-  exit;
-}
-
-$file_dir  = sprintf("%s/../%s/current", dirname(__FILE__), $app_name);
-$file_path = "$file_dir/$file";
-if(!@stat($file_path)) {
-  // if the user is logged in, but the url passed doesn't translate to a file,
-  // then redirect to index.php
-  header('Location: ' . str_replace($_SERVER["PATH_INFO"], "", $_SERVER['SCRIPT_URI']) . '/index.php');
-  exit(0);
-}
-
-$_SERVER["PATH_INFO"] = $_SERVER["SCRIPT_NAME"];
-$_SERVER["SCRIPT_NAME"] = $file;
-
-set_include_path(sprintf("%s:%s", $file_dir, get_include_path()));
-chdir($file_dir);
-
-session_write_close(); // phpmyadmin is picky...it wants to open it's own session or returns error
-require_once($file_path);
 ?>
