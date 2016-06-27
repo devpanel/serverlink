7974c49f381ee276bb14589e9d4d519dcc505e93
Vinicius Mello <vmmello@webenabled.com>
2016-06-27 00:52:11 -0300

    removed compat/w_/public_html/cgi/phpmyadmin/

--- compat/w_/public_html/cgi/phpmyadmin/3.3.1/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$i = 0;
-$i++;
-$cfg['Servers'][$i]['extension']     = 'mysqli';
-$cfg['Servers'][$i]['auth_type']     = 'signon';
-$cfg['Servers'][$i]['SignonSession'] = 'DPSignonSession';
-$cfg['Servers'][$i]['SignonURL']     = 'dp_signon.php';
-$cfg['Servers'][$i]['LogoutURL']     = 'dp_logout.php';
-?>
