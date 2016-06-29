d5977f747da627f053d33490cbfa7500b8cceb73
Vinicius Mello <vmmello@webenabled.com>
2016-06-28 23:40:20 -0300

    phpmyadmin: removed current/ link

--- src/packages/phpmyadmin/devpanel_auth.php
+++ src/packages/phpmyadmin/devpanel_auth.php
@@ -1,7 +1,7 @@
 <?php
 
 $curr_path = dirname(__FILE__);
-require_once($curr_path . "/../../../../../../lib/php/webapp_token_access.inc.php");
+require_once($curr_path . "/../../../../../lib/php/webapp_token_access.inc.php");
 
 $app_name = "phpmyadmin";
 
