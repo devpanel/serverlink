a4868bc2f2ff9874d50b754dc4ced6d0e5e569c8
Vinicius Mello <vmmello@webenabled.com>
2016-08-02 22:57:10 -0300

    fixed bootstrap for CentOS 7
    checked whether perl module Data::Dumper is installed and install it.

--- install/bootstrap/bootstrap.centos.sh
+++ install/bootstrap/bootstrap.centos.sh
@@ -4,4 +4,13 @@ bootstrap_centos() {
   yum -y install git perl perl-devel perl-CGI perl-Time-HiRes \
     perl-Digest-HMAC perl-Digest-SHA perl-Crypt-SSLeay \
     redhat-lsb-core
+  
+  # test whether CGI::Util is available, it's needed by taskd
+  # from Ubuntu 16 it's not included in the perl distribution
+  perl -MData::Dumper -e 'exit 0;' &>/dev/null
+  if [ $? -eq 0 ]; then
+    :
+  else
+    yum -y install perl-Data-Dumper
+  fi
 }
