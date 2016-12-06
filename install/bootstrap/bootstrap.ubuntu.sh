7673a364cda317be8dc0770f7d3466f29b3b8f94
S M Asad Rahman <asad.rahman@sekai-lab-bd.com>
2016-12-06 21:50:26 +0600

    Node js & npm install script for ubuntu

--- install/bootstrap/bootstrap.ubuntu.sh
+++ install/bootstrap/bootstrap.ubuntu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-bootstrap_ubuntu() {
+#bootstrap_ubuntu() {
   export DEBIAN_FRONTEND='noninteractive'
   apt-get update
   apt-get -y install libcrypt-ssleay-perl libjson-xs-perl ca-certificates \
              git libio-socket-ssl-perl liburi-perl
-}
+
+  apt-get -qq update
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  apt-get install nodejs -y
+#}
