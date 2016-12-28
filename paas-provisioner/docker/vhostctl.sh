ce79b24aad1c4caa284042be3b630e8e93d25300
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2016-12-28 15:14:31 +0100

    AWS workaround for hostname setup delay

--- paas-provisioner/docker/vhostctl.sh
+++ paas-provisioner/docker/vhostctl.sh
@@ -170,6 +170,10 @@ if [ `ip ad sh|grep -c ' eth'` -gt '0' ]; then
 else
   vps_ip=`ip ad sh|grep ' ens'|tail -1|awk '{print $2}'|awk -F'/' '{print $1}'`
 fi
+while [ `hostname|grep -c devpanel.net` -eq 0 ]; do
+  echo "Waiting for hostname setup..."
+  sleep 1
+done
 hostname_fqdn=`hostname`
 
 
