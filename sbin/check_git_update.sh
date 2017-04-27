05cc81ea1c5d263e0b1ca5c480791a13214106e4
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2017-04-27 03:04:16 +0200

    get_app_info extended

--- sbin/check_git_update.sh
+++ sbin/check_git_update.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cd /opt/webenabled && git rev-list HEAD...origin/master --count
+IFS=!
+LOCAL_APPS_ARRAY=(`find /home/clients/websites/* -maxdepth 0 -type d -printf %f!`)
+for username in ${LOCAL_APPS_ARRAY[*]}; do
+  username=`echo $username|cut -d '_' -f2`
+  git_commits=$(cd /home/clients/websites/w_${username}/public_html/${username} && git rev-list HEAD...origin/master --count 2>&1)
+  if [ $(echo $git_commits|grep fatal|wc -l) -gt 0 ]; then git_commits=-1; fi
+  echo w_${username},${git_commits}
+done
