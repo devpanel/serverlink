1c292747063ffbd207e3ac429044c5b0fcd05d1e
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2017-04-28 00:04:47 +0200

    check app git updates in docker containers also

--- sbin/check_git_update.sh
+++ sbin/check_git_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# local LOCAL_APPS_ARRAY
 IFS=!
 LOCAL_APPS_ARRAY=(`find /home/clients/websites/* -maxdepth 0 -type d -printf %f!`)
 for username in ${LOCAL_APPS_ARRAY[*]}; do
@@ -8,3 +9,12 @@ for username in ${LOCAL_APPS_ARRAY[*]}; do
   if [ $(echo $git_commits|grep fatal|wc -l) -gt 0 ]; then git_commits=-1; fi
   echo w_${username},${git_commits}
 done
+
+# docker apps
+if [ $(whereis docker|grep -c /usr) -gt 0 ]; then
+  IFS=$'\n'
+  DOCKER_APPS_ARRAY=(`docker ps|tail -n +2|awk '{print $NF}'`)
+  for docker_container in ${DOCKER_APPS_ARRAY[*]}; do
+    docker exec ${docker_container} /opt/webenabled/sbin/check_git_update.sh
+  done
+fi
