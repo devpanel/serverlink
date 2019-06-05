2adb90a639dabc060eb6dfa68c249622a813d4aa
Vinicius Mello <vmmello@webenabled.com>
2019-06-04 23:10:16 -0300

    added tests for building seed apps
    also added minor improvements to testing.

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 self_bin=$(readlink -e "${BASH_SOURCE[0]}")
 if [ $? -ne 0 ]; then
@@ -7,6 +8,12 @@ if [ $? -ne 0 ]; then
 fi
 
 self_dir="${self_bin%/*}"
+sys_dir=${self_dir%/*/*}
+test_conf_ini="$self_dir/defaults.ini"
+
+. $sys_dir/lib/functions
+
+read_ini_file_into_namespace "$test_conf_ini" tests
 
 declare -a cmd_args_ar=()
 while [ -n "$1" ]; do
@@ -25,5 +32,17 @@ else
     cmd_args_ar+=( "$self_dir/tests.clitest" )
   fi
 fi
+
+for var in ${!tests__*}; do
+  export $var
+done
+
+DEVPANEL_HOME="$sys_dir"
+SEEDAPPS_DIR="${self_dir%/*}/seedapps"
+TEST_ID=$(gen_random_str_az09_lower 3)
+
+export DEVPANEL_HOME SEEDAPPS_DIR TEST_ID
+
+echo "=== Starting testing session: $TEST_ID   ($(date))"
 
 "$self_dir/clitest" "${cmd_args_ar[@]}"
