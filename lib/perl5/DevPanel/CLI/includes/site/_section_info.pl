74e21d900b12a921aae4d65e00176df917d7639b
Vinicius Mello <vmmello@webenabled.com>
2019-09-17 18:14:31 -0300

    removed a few old and unused features
    * removed old AWS cluster of Drupal 7
    * removed files of old CLI

--- lib/perl5/DevPanel/CLI/includes/site/_section_info.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-#
-my $ref = {
-  'short_txt' => 'Operations on locally installed sites',
-  'main_txt'  => 'Operations on locally installed sites',
-};
--- lib/perl5/DevPanel/CLI/includes/system/_section_info.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-my $ref = {
-  short_txt => 'Operations on DevPanel local software installation and configuration',
-  main_txt  => 'Operations on DevPanel local software installation and configuration',
-};
